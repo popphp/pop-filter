5b1ccc6ad8bfa471ff451309d034c5a440f3d412
Nick Sagona <nick@nolainteractive.com>
2020-02-11 13:10:47 -0600

    Update version

--- src/Filter.php
+++ src/Filter.php
@@ -21,7 +21,7 @@ namespace Pop\Filter;
  * @author     Nick Sagona, III <dev@nolainteractive.com>
  * @copyright  Copyright (c) 2009-2020 NOLA Interactive, LLC. (http://www.nolainteractive.com)
  * @license    http://www.popphp.org/license     New BSD License
- * @version    3.0.0
+ * @version    3.1.0
  */
 class Filter extends AbstractFilter
 {
